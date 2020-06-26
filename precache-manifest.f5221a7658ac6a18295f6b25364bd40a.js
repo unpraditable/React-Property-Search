92a921fe1c8d7fb578c629b68fd3b61997ab6e5a
Aditya Pradita S <pradita397@gmail.com>
2020-06-27 00:51:37 +0700

    Updates

---
+++ precache-manifest.f5221a7658ac6a18295f6b25364bd40a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ec2f7b9ba2b4fcac9cb2a81b8c6754f",
+    "url": "/react-property-search/index.html"
+  },
+  {
+    "revision": "507343ad572b5c0f579e",
+    "url": "/react-property-search/static/css/2.d105ccba.chunk.css"
+  },
+  {
+    "revision": "2ea48631c1307c406c68",
+    "url": "/react-property-search/static/css/main.e406e999.chunk.css"
+  },
+  {
+    "revision": "507343ad572b5c0f579e",
+    "url": "/react-property-search/static/js/2.ccdcdcef.chunk.js"
+  },
+  {
+    "revision": "6791e7fb9584d65ed87d655035c7da95",
+    "url": "/react-property-search/static/js/2.ccdcdcef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ea48631c1307c406c68",
+    "url": "/react-property-search/static/js/main.279a4675.chunk.js"
+  },
+  {
+    "revision": "25f130df10cebb61b16e",
+    "url": "/react-property-search/static/js/runtime-main.733e2663.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-property-search/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/react-property-search/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/react-property-search/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/react-property-search/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/react-property-search/static/media/slick.f97e3bbf.svg"
+  }
+]);
